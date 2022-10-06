874af1bf79b9a99a20cd88abd16c87760693a9d2
haofang <haofang97@gmail.com>
2022-10-06 09:37:55 +0800

    added highlights to html

--- src/components/AllGamesStats.js
+++ src/components/AllGamesStats.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import SearchContext from "../context/searchContext";
 import GameData from "./GameData";
+import Highlights from "./Highlights";
 
 const AllGamesStats = () => {
   const searchContext = useContext(SearchContext);
@@ -9,14 +10,19 @@ const AllGamesStats = () => {
   // Return
   // ======
   return (
-    <div className="allGamesStats">
-      <h3 className="statsHeader">
-        Total Stats ({searchContext.allIndividualGames.current.length} Games)
-      </h3>
-      <GameData
-        numberOfGames={searchContext.allIndividualGames.current.length}
-        stats={searchContext.totalStats}
-      />
+    <div className="flexbox allGamesStats">
+      <div classname="allGamesStats-left">
+        <h3 className="statsHeader">
+          Total Stats ({searchContext.allIndividualGames.current.length} Games)
+        </h3>
+        <GameData
+          numberOfGames={searchContext.allIndividualGames.current.length}
+          stats={searchContext.totalStats}
+        />
+      </div>
+      <div className="allGamesStats-right">
+        <Highlights />
+      </div>
     </div>
   );
 };
